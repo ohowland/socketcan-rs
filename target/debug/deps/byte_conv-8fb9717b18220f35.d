c53c469fc2a9e16171e8d55913392ce71427ed7b
Owen <ohowland@gmail.com>
2020-09-24 18:22:28 -0400

    roll back

--- target/debug/deps/byte_conv-8fb9717b18220f35.d
+++
@@ -1,7 +0,0 @@
-/home/owen/dev/socketcan-rs/target/debug/deps/byte_conv-8fb9717b18220f35.rmeta: /home/owen/.cargo/registry/src/github.com-1ecc6299db9ec823/byte_conv-0.1.1/src/lib.rs
-
-/home/owen/dev/socketcan-rs/target/debug/deps/libbyte_conv-8fb9717b18220f35.rlib: /home/owen/.cargo/registry/src/github.com-1ecc6299db9ec823/byte_conv-0.1.1/src/lib.rs
-
-/home/owen/dev/socketcan-rs/target/debug/deps/byte_conv-8fb9717b18220f35.d: /home/owen/.cargo/registry/src/github.com-1ecc6299db9ec823/byte_conv-0.1.1/src/lib.rs
-
-/home/owen/.cargo/registry/src/github.com-1ecc6299db9ec823/byte_conv-0.1.1/src/lib.rs:
--- target/debug/deps/hex-d7a023c483ddbb74.d
+++
@@ -1,7 +0,0 @@
-/home/owen/dev/socketcan-rs/target/debug/deps/hex-d7a023c483ddbb74.rmeta: /home/owen/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.2.0/src/lib.rs
-
-/home/owen/dev/socketcan-rs/target/debug/deps/libhex-d7a023c483ddbb74.rlib: /home/owen/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.2.0/src/lib.rs
-
-/home/owen/dev/socketcan-rs/target/debug/deps/hex-d7a023c483ddbb74.d: /home/owen/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.2.0/src/lib.rs
-
-/home/owen/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.2.0/src/lib.rs:
