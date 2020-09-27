a3eceafb637ba253f12de1d2455e06a857967341
Owen <ohowland@gmail.com>
2020-09-27 18:32:25 -0400

    log 2nd attempt

--- examples/write_once.rs
+++ examples/write_once.rs
@@ -1,15 +1,17 @@
 extern crate socketcan;
+extern crate log;
 
 use socketcan::{CanSocket, CanFrame};
 
 fn main() {
+    
     let bus  = CanSocket::open("vcan0").unwrap();
 
     let data: [u8; 4] = [222, 173, 190, 239];
     let id: u32 = 123;
     let frame = CanFrame::new(id, &data, false, false).unwrap();
     match bus.write(&frame) {
-        Ok(()) => println!("Frame Send Success"),
-        Err(e) => println!("Frame Send Error {}", e),
+        Ok(()) => log::debug!("Frame Send Success"),
+        Err(e) => log::debug!("Frame Send Error {}", e),
     }
 }
